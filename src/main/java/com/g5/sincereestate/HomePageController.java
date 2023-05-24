fb2bc6e5bf9a65d3b25b53a5cca448034dc63657
EnesAkman <enessakmann12@hotmail.com>
2023-05-24 22:51:07 +0300

    enesA

--- src/main/java/com/g5/sincereestate/HomePageController.java
+++ src/main/java/com/g5/sincereestate/HomePageController.java
@@ -1,4 +1,29 @@
 package com.g5.sincereestate;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
 public class HomePageController {
+
+    @FXML
+    private TableColumn<?, ?> adDate;
+
+    @FXML
+    private TableColumn<?, ?> location;
+
+    @FXML
+    private TableColumn<?, ?> price;
+
+    @FXML
+    private TableColumn<?, ?> propertyStatus;
+
+    @FXML
+    private TableColumn<?, ?> propertyType;
+
+
+    public void table(){
+
+    }
+
 }
+
---
+++ src/main/java/com/g5/sincereestate/Property.java
@@ -0,0 +1,8 @@
+package com.g5.sincereestate;
+
+import javafx.beans.property.StringProperty;
+
+public class Property {
+    private final StringProperty
+
+}
